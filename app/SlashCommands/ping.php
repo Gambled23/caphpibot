f45a64ce443012e17e464d7267ecdbcf1b904958
Gambled23 <ipog71@gmail.com>
2024-02-28 09:37:39 -0600

    xp: pruebas de choices

--- app/SlashCommands/ping.php
+++ app/SlashCommands/ping.php
@@ -2,6 +2,7 @@
 
 namespace App\SlashCommands;
 use Discord\Parts\Interactions\Command\Option;
+use Discord\Parts\Interactions\Command\Choice;
 use Laracord\Commands\SlashCommand;
 
 class ping extends SlashCommand
@@ -49,12 +50,41 @@ class ping extends SlashCommand
      */
     public function handle($interaction)
     {
+        $respuesta = $interaction->data->options['respuesta']->value;
+
         $interaction->respondWithMessage(
             $this
               ->message()
               ->title('ping')
-              ->content('Hello world!')
+              ->content('You chose: ' . $respuesta)
               ->build()
         );
+    }
+
+    public function options()
+    {
+        $option = new Option($this->discord());
+        $choice1 = new Choice($this->discord());
+        $choice1->setName('Pato')->setValue('pato');
+        $choice2 = new Choice($this->discord());
+        $choice2->setName('Peto')->setValue('peto');
+        $choice3 = new Choice($this->discord());
+        $choice3->setName('Pito')->setValue('pito');
+        $choice4 = new Choice($this->discord());
+        $choice4->setName('Poto')->setValue('poto');
+        $choice5 = new Choice($this->discord());
+        $choice5->setName('Xavier')->setValue('xavier');
+
+        return [
+            $option
+                ->setName('respuesta')
+                ->setDescription('la respuesta que dará el bot al comando /ping')
+                ->setType(Option::STRING)
+                ->addChoice($choice1)
+                ->addChoice($choice2)
+                ->addChoice($choice3)
+                ->addChoice($choice4)
+                ->addChoice($choice5)
+        ];
     }
 }
